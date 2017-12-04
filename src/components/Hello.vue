628bf4520fcc9fbbc5364645e2b9031ccd77577a
Stefano Guandalini <guandalf@gmail.com>
2017-12-04 21:37:53 +0100

    Lots of cleanup and a new component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -59,27 +59,12 @@
       <router-view /> component
       if using subRoutes
     -->
-    <q-list highlight>
-      <q-list-header>Recent ratings</q-list-header>
-      <q-item v-for="r in items">
-        <q-item-side>
-          <q-item-tile label><big>{{r.platenum}}</big></q-item-tile>
-          <q-item-tile sublabel>
-            <q-rating slot="subtitle" v-model="r.platerating" :max="5" />
-          </q-item-tile>
-        </q-item-side>
-        <q-item-main>
-          <q-item-tile label>{{r.platecomment}}</q-item-tile>
-        </q-item-main>
-      </q-item>
-    </q-list>
+    <ratings-list/>
   </q-layout>
 </template>
 
 <script>
 import {
-  dom,
-  event,
   QLayout,
   QToolbar,
   QToolbarTitle,
@@ -96,24 +81,7 @@ import {
   QField
 } from 'quasar'
 import { db } from '../firebase'
-
-const
-  { viewport } = dom,
-  { position } = event,
-  moveForce = 30,
-  rotateForce = 40,
-  RAD_TO_DEG = 180 / Math.PI
-
-function getRotationFromAccel (accelX, accelY, accelZ) {
-  /* Reference: http://stackoverflow.com/questions/3755059/3d-accelerometer-calculate-the-orientation#answer-30195572 */
-  const sign = accelZ > 0 ? 1 : -1
-  const miu = 0.001
-
-  return {
-    roll: Math.atan2(accelY, sign * Math.sqrt(Math.pow(accelZ, 2) + miu * Math.pow(accelX, 2))) * RAD_TO_DEG,
-    pitch: -Math.atan2(accelX, Math.sqrt(Math.pow(accelY, 2) + Math.pow(accelZ, 2))) * RAD_TO_DEG
-  }
-}
+import RatingsList from './RatingsList.vue'
 
 export default {
   name: 'index',
@@ -131,7 +99,8 @@ export default {
     QItemTile,
     QInput,
     QRating,
-    QField
+    QField,
+    RatingsList
   },
   data () {
     return {
@@ -139,29 +108,11 @@ export default {
         platenum: '',
         platerating: 0,
         platecomment: ''
-      },
-      orienting: window.DeviceOrientationEvent && !this.$q.platform.is.desktop,
-      rotating: window.DeviceMotionEvent && !this.$q.platform.is.desktop,
-      moveX: 0,
-      moveY: 0,
-      rotateY: 0,
-      rotateX: 0
+      }
     }
   },
   firebase: {
     items: db.ref('scorching-heat-9667')
-  },
-  computed: {
-    position () {
-      const transform = `rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg)`
-      return {
-        top: this.moveY + 'px',
-        left: this.moveX + 'px',
-        '-webkit-transform': transform,
-        '-ms-transform': transform,
-        transform
-      }
-    }
   },
   methods: {
     addRating () {
@@ -169,74 +120,6 @@ export default {
       this.newRating.platenum = ''
       this.newRating.platerate = 0
       this.newRating.platecomment = ''
-    },
-    move (evt) {
-      const
-        {width, height} = viewport(),
-        {top, left} = position(evt),
-        halfH = height / 2,
-        halfW = width / 2
-
-      this.moveX = (left - halfW) / halfW * -moveForce
-      this.moveY = (top - halfH) / halfH * -moveForce
-      this.rotateY = (left / width * rotateForce * 2) - rotateForce
-      this.rotateX = -((top / height * rotateForce * 2) - rotateForce)
-    },
-    rotate (evt) {
-      if (evt.rotationRate &&
-          evt.rotationRate.beta !== null &&
-          evt.rotationRate.gamma !== null) {
-        this.rotateX = evt.rotationRate.beta * 0.7
-        this.rotateY = evt.rotationRate.gamma * -0.7
-      }
-      else {
-        /* evt.acceleration may be null in some cases, so we'll fall back
-           to evt.accelerationIncludingGravity */
-        const
-          accelX = evt.acceleration.x || evt.accelerationIncludingGravity.x,
-          accelY = evt.acceleration.y || evt.accelerationIncludingGravity.y,
-          accelZ = evt.acceleration.z || evt.accelerationIncludingGravity.z - 9.81,
-          rotation = getRotationFromAccel(accelX, accelY, accelZ)
-
-        this.rotateX = rotation.roll * 0.7
-        this.rotateY = rotation.pitch * -0.7
-      }
-    },
-    orient (evt) {
-      if (evt.beta === null || evt.gamma === null) {
-        window.removeEventListener('deviceorientation', this.orient, false)
-        this.orienting = false
-
-        window.addEventListener('devicemotion', this.rotate, false)
-      }
-      else {
-        this.rotateX = evt.beta * 0.7
-        this.rotateY = evt.gamma * -0.7
-      }
-    }
-  },
-  mounted () {
-    this.$nextTick(() => {
-      if (this.orienting) {
-        window.addEventListener('deviceorientation', this.orient, false)
-      }
-      else if (this.rotating) {
-        window.addEventListener('devicemove', this.rotate, false)
-      }
-      else {
-        document.addEventListener('mousemove', this.move)
-      }
-    })
-  },
-  beforeDestroy () {
-    if (this.orienting) {
-      window.removeEventListener('deviceorientation', this.orient, false)
-    }
-    else if (this.rotating) {
-      window.removeEventListener('devicemove', this.rotate, false)
-    }
-    else {
-      document.removeEventListener('mousemove', this.move)
     }
   }
 }
---
+++ src/components/RatingsList.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-list highlight>
+    <q-list-header>Recent ratings</q-list-header>
+    <q-item v-for="r in items" :key="r.id">
+      <q-item-side>
+        <q-item-tile label><big>{{r.platenum}}</big></q-item-tile>
+        <q-item-tile sublabel>
+          <q-rating slot="subtitle" v-model="r.platerating" :max="5" />
+        </q-item-tile>
+      </q-item-side>
+      <q-item-main>
+        <q-item-tile label>{{r.platecomment}}</q-item-tile>
+      </q-item-main>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+import {
+  QList,
+  QListHeader,
+  QItem,
+  QItemSide,
+  QItemMain,
+  QItemTile,
+  QRating,
+  QField
+} from 'quasar'
+import { db } from '../firebase'
+
+export default {
+  name: 'ratings-list',
+  data () {
+    return {}
+  },
+  components: {
+    QList,
+    QListHeader,
+    QItem,
+    QItemSide,
+    QItemMain,
+    QItemTile,
+    QRating,
+    QField
+  },
+  firebase: {
+    items: db.ref('scorching-heat-9667')
+  }
+}
+</script>
+
+<style>
+</style>
